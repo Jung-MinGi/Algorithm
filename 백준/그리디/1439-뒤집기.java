303396cc0576e3d00e45623b4a8c6e467105a40d
mg0411 <118701129+mg0411@users.noreply.github.com>
2023-01-29 15:40:41 +0900

    Create 1439-뒤집기.java

---
+++ 백준/그리디/1439-뒤집기.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String n = br.readLine();
+		int[] arr = new int[2];
+		for (int i = 0; i < n.length() - 1; i++) {
+			if (n.charAt(i) != n.charAt(i + 1)) {
+				arr[n.charAt(i) - '0']++;
+			}
+			if (i == n.length() - 2) {
+				if (n.charAt(i) != n.charAt(i + 1)) {
+					arr[n.charAt(i + 1) - '0']++;
+				}else {
+						arr[n.charAt(i)-'0']++;
+					}
+				}
+		}
+		if (arr[0] == arr[1]) {
+			System.out.println(arr[0]);
+		} else {
+			System.out.println(Math.min(arr[0], arr[1]));
+		}
+	}
+}
+/*
+ * 인풋데이터의 크기가 최대 10^6이다
+ * 이중포문O(N^2)사용X 
+ */
