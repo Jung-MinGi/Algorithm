aaa5d5d076ddf6a591df140f6bee26464008cdd8
mg0411 <118701129+mg0411@users.noreply.github.com>
2023-02-02 10:47:04 +0900

    Create 2751-수 정렬하기2.java

---
+++ 백준/정렬/2751-수 정렬하기2.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int[] arr = new int[2000001];
+		for (int i = 0; i < n; i++) {
+			int a = Integer.parseInt(br.readLine());
+			arr[a + 1000000]++;
+		}
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] == 1) {
+			sb.append(i-1000000);
+			sb.append('\n');
+			}
+			
+		}System.out.println(sb);
+	}
+}
+/*
+*최악의 데이터 크기가 10^6이니깐 O(N)이하로 봐야되고
+*데이터의 개수와 범위를 봐서 카운팅 정렬을 사용해도 메모리 낭비는 없을거 같음ㅁ
+*그리고 마지막에 출력에서 처음에  STRINGBUILDER을 쓰지 않아 시간초과 났음
+*/
