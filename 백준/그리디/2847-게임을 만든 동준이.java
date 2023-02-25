6f1b16234f1144545fed50d015f3f372423e1c6d
mg0411 <118701129+mg0411@users.noreply.github.com>
2023-02-25 12:35:26 +0900

    Create 2847-게임을 만든 동준이.java

---
+++ 백준/그리디/2847-게임을 만든 동준이.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int[] arr = new int[n];
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		int cnt=0;
+		for(int i = arr.length-1 ; i > 0 ; i--) {
+			if(arr[i] <= arr[i-1]) {
+				cnt+=arr[i-1]-(arr[i]-1);
+				arr[i-1] = arr[i]-1;
+			}else continue;
+		}
+		System.out.println(cnt);
+
+	}
+}
+/*
+*뒤에서부터 풀어볼 생각
+*시간제한 1초*1천만번
+*데이터 크기와 단계가 비례하므로 선형시간을  
+*/
