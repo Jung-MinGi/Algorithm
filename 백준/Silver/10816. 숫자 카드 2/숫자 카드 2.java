0b0b8fb34edf5cffc418e0ee3f98915abb154c8e
MinGi-Jung <118701129+Jung-MinGi@users.noreply.github.com>
2023-06-06 13:22:03 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1476 ms, Memory: 125484 KB -BaekjoonHub

---
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int[] ref;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        ref =new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i=0 ; i<n; i++){
+            ref[i]=Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(ref);
+        int a = Integer.parseInt(br.readLine());
+        int[] arr =new int[a];
+        st = new StringTokenizer(br.readLine());
+        for(int i=0 ; i<a; i++){
+            arr[i]=Integer.parseInt(st.nextToken());
+        }
+
+        ///
+        StringBuilder sb = new StringBuilder();
+        for (int i : arr) {
+           sb.append(upperBound(i)-lowerBound(i)).append(' ');
+        }
+        System.out.print(sb);
+
+    }
+   static int lowerBound(int target){
+        int low = 0;
+        int high = ref.length;
+        while(low<high){
+            int mid = (low+high)/2;
+           if(ref[mid] >= target){
+               high = mid;
+           }else{
+               low = mid + 1;
+           }
+        }
+        return low;
+    }
+    static int upperBound(int target){
+        int low = 0;
+        int high = ref.length;
+        while(low<high){
+            int mid = (low+high)/2;
+           if(ref[mid] <= target){
+               low = mid + 1;
+           }else{
+               high = mid;
+           }
+        }
+        return low;
+    }
+}
