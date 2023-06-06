79be83f7eb1cbb376f8bd41aa712efccb5884562
MinGi-Jung <118701129+Jung-MinGi@users.noreply.github.com>
2023-06-06 12:34:37 +0900

    [Silver IV] Title: 수 찾기, Time: 624 ms, Memory: 45932 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,4 +1,3 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -6,42 +5,44 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
+    static int[] ref;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n];
+        ref =new int[n];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i = 0 ; i < arr.length ; i++){
+        for(int i=0 ; i<n; i++){
+            ref[i]=Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(ref);
+        int a = Integer.parseInt(br.readLine());
+        int[] arr =new int[a];
+        st = new StringTokenizer(br.readLine());
+        for(int i=0 ; i<a; i++){
             arr[i]=Integer.parseInt(st.nextToken());
         }
-        int m = Integer.parseInt(br.readLine());
-        st = new StringTokenizer(br.readLine());
-        int[] brr = new int[m];
-        for(int i = 0 ; i < brr.length ; i++){
-            brr[i]=Integer.parseInt(st.nextToken());
+        StringBuilder sb = new StringBuilder();
+        for (int i : arr) {
+            sb.append(binarySearch(i)).append('\n');
         }
-        Arrays.sort(arr);
-        for(int i : brr){
-            boolean result = binarySearch(arr,i);
-            if(result){
-                System.out.println(1);
-            }else System.out.println(0);
-        }
+        System.out.print(sb);
+
     }
-    static boolean binarySearch(int[] a, int b){
+   static int binarySearch(int target){
         int low = 0;
-        int high = a.length-1;
+        int high = ref.length-1;
         while(low<=high){
             int mid = (low+high)/2;
-            if(a[mid] == b){
-                return true;
-            }else if(a[mid] <= b){
-                low = mid + 1 ;
+            if(ref[mid] == target){
+
+                return 1;
+
+            }else if(ref[mid] > target){
+                high = mid -1;
             }else{
-                high = mid - 1 ;
+                low = mid + 1;
             }
         }
-        return false;
+        return 0;
     }
 }
-
