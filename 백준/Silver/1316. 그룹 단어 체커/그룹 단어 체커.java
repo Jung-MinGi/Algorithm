be1652635cd15620e8e625db1e45db79ea84d472
MinGi-Jung <118701129+Jung-MinGi@users.noreply.github.com>
2023-06-07 12:11:23 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 128 ms, Memory: 14264 KB -BaekjoonHub

---
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int error=0;
+		for(int i = 0 ; i < n ; i++) {
+			String str = br.readLine();
+			int[] arr = new int[26];
+			for(int j = 0 ; j < str.length() ; j++) {
+				if(arr[str.charAt(j)-'a'] == 0) {
+					arr[str.charAt(j)-'a']++;
+				}else {
+					if(str.charAt(j-1)==str.charAt(j)) {//바로 직전에 같은 값인 경우
+						continue;
+					}else {
+						error++;
+						break;
+					}
+				}
+			}
+		}
+		System.out.println(n-error);
+	}
+}
