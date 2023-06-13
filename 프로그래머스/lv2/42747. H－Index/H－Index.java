7dee02f8b0d36b4da5a65636fcc877718c661cbf
MinGi-Jung <118701129+Jung-MinGi@users.noreply.github.com>
2023-06-13 12:38:15 +0900

    [level 2] Title: H-Index, Time: 0.33 ms, Memory: 71.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42747. H－Index/H－Index.java
+++ 프로그래머스/lv2/42747. H－Index/H－Index.java
@@ -2,15 +2,15 @@ import java.util.*;
 class Solution {
     public int solution(int[] citations) {
         int answer = 0;
-        int h=10001;
+       // int h=10001;
         Arrays.sort(citations);
         for(int i=citations.length-1; i>=0; i--){
             
             if(citations[i] >= answer){
                 answer++;
-                h=citations[i];
+                answer=Math.min(answer,citations[i]);
             }
         }
-        return Math.min(answer,h);
+        return answer;
     }
 }
