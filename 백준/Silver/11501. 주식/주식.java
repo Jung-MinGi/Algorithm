43a9f99b14aad7d3c6df74d44c3615a277337631
MinGi-Jung <118701129+Jung-MinGi@users.noreply.github.com>
2023-06-15 14:15:09 +0900

    [Silver II] Title: 주식, Time: 1160 ms, Memory: 310628 KB -BaekjoonHub

--- 백준/Silver/11501. 주식/주식.java
+++ 백준/Silver/11501. 주식/주식.java
@@ -13,51 +13,27 @@ public class Main {
         int k;
         long sum;
         StringTokenizer st;
-        //StringBuilder sb = new StringBuilder();
+        StringBuilder sb = new StringBuilder();
         while (n-- > 0) {
             sum = 0;
             k = Integer.parseInt(br.readLine());
             st = new StringTokenizer(br.readLine());
             arr = new long[k];
-            long[] max = new long[k];
-            long val = -1;
             for (int i = 0; i < k; i++) {
                 arr[i] = Integer.parseInt(st.nextToken());
-                if(val <= arr[i]){
-                    val = arr[i];
-                }else{
-                    max[i-1] = val;
-                    val = arr[i];
-                }
             }
-            if(arr[arr.length-2] <= arr[arr.length-1]){
-                max[arr.length-1] = arr[arr.length-1];
-            }
-            long big=max[max.length-1];
-            for(int i=max.length-2; i>=0; i--){
-                if(big >= max[i]){
-                    max[i]=0;
-                }else{
-                    big = max[i];
-                }
-            }
-            int temp=0;
-            //max배열 완성
-            for(int i=0; i<max.length; i++){
-                if(max[i]!=0){
 
-                    for(int j=temp; j<i; j++){
-                        sum = sum+(max[i]-arr[j]);
-                    }
-                    temp=i+1;
+            long big=0;
+            for(int i=arr.length-1; i>=0; i--){
+                if(arr[i] > big){
+                    big =arr[i];
+                }else{
+                    sum+=(big-arr[i]);
                 }
             }
-            //sb.append(sum).append('\n');
-//            for (long i : max) {
-//                System.out.println("max배열 = " + i);
-//            }
 
-            System.out.println(sum);
+            sb.append(sum).append('\n');
         }
+        System.out.println(sb);
     }
 }
