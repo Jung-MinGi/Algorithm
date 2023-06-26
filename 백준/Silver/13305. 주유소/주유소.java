1af4f73b62be0b2f747204940af8a0ee40f5e682
MinGi-Jung <118701129+Jung-MinGi@users.noreply.github.com>
2023-06-26 12:22:19 +0900

    [Silver III] Title: 주유소, Time: 364 ms, Memory: 30600 KB -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] len = new int[n-1];
+        for(int i=0; i<len.length; i++){
+            len[i]=Integer.parseInt(st.nextToken());
+        }
+        st = new StringTokenizer(br.readLine());
+        int[] price= new int[n];
+        for(int i=0; i<price.length; i++){
+            price[i]=Integer.parseInt(st.nextToken());
+        }
+        long answer=0;
+        int minPrice=Integer.MAX_VALUE;
+        for(int i=0; i<len.length; i++){
+                if(price[i] < minPrice){
+                    minPrice=price[i];
+                }
+            answer = answer+(minPrice*len[i]);
+        }
+        System.out.println(answer);
+    }
+}
+
