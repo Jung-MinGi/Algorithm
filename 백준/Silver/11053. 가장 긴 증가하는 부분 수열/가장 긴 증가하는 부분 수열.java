65a6d3c448ae36c6323d54797fa9326574cfa488
MinGi-Jung <118701129+Jung-MinGi@users.noreply.github.com>
2023-07-10 15:05:48 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 132 ms, Memory: 14332 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -4,33 +4,47 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    static int[] dp;
+    static int[] bs;
     static int[] arr;
     static int n;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
-        dp = new int[n];
+        bs = new int[n];
         arr = new int[n];
         StringTokenizer st = new StringTokenizer(br.readLine());
         for(int i=0; i<n; i++){
             arr[i]=Integer.parseInt(st.nextToken());
         }
-        dp[0]=1;
-        int max=0;
+        int cnt=0;
+        bs[cnt]=arr[0];
+
         for(int i=1; i<n; i++){
-            for(int j=0; j<i; j++){
-                if(arr[i] > arr[j]){
-                    dp[i] = Math.max(dp[j],dp[i]);
-                }
+            if(bs[cnt] < arr[i]){
+                bs[++cnt]=arr[i];
+            }else{
+                int a = binarySearch(arr[i], cnt);
+                bs[a]=arr[i];
             }
-            dp[i]++;
-            max = Math.max(dp[i],max);
         }
-        if(n==1) max=1;
-        System.out.println(max);
-       
+        int answer=0;
+        for (int b : bs) {
+           if(b!=0) answer++;
+        }
+        System.out.println(answer);
     }
+    static int binarySearch(int target,int cnt){
+        int low=0;
+        int high=cnt;
+        while(low<high){
+            int mid = (low+high)/2;
+            if(bs[mid] >= target){
+                high = mid;
+            }else low = mid+1;
+        }
+    return low;
+    }
+
 
 }
 
