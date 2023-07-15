0e46cb9a62fe98662c84bf34872f9c78266e28cc
MinGi-Jung <118701129+Jung-MinGi@users.noreply.github.com>
2023-07-15 13:31:37 +0900

    [Silver III] Title: N과 M (2), Time: 128 ms, Memory: 14204 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static int[] arr;
+    public static boolean[] visit;
+    static int n,m;
+
+    public static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr = new int[m];
+        visit = new boolean[n+1];
+        dfs(0);
+        System.out.println(sb);
+    }
+    static void dfs(int depth){
+
+
+        if(depth == m){//재귀 탈출
+            for (int i : arr) {
+                sb.append(i).append(' ');
+            }
+            sb.append('\n');
+            return;
+        }
+
+        for(int i=1; i<=n; i++){
+            if(depth>0 && arr[depth-1] > i){
+                continue;
+            }
+            if(!visit[i]){
+                visit[i]=true;
+                arr[depth]=i;
+                dfs(depth+1);
+                visit[i]=false;
+            }
+        }
+    }
+}
