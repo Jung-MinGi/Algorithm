c6d064fc01c3959a0d2ce9798fe3bd343aa53c80
MinGi-Jung <118701129+Jung-MinGi@users.noreply.github.com>
2023-07-17 18:30:14 +0900

    [Silver III] Title: 모든 순열, Time: 260 ms, Memory: 22344 KB -BaekjoonHub

---
+++ 백준/Silver/10974. 모든 순열/모든 순열.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class Main {
+    static int n;
+    static int[] arr;
+    static boolean[] visit;
+   static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n];
+        visit = new boolean[n];
+        dfs(0);
+        System.out.println(sb);
+        }
+    static void dfs(int depth){
+        if(depth == n){
+            for (int i : arr) {
+                sb.append(i).append(' ');
+            }sb.append('\n');
+            return;
+        }
+
+        for(int i=0; i<n; i++){
+            if (!visit[i]) {
+                arr[depth]=i+1;
+                visit[i]=true;
+                dfs(depth+1);
+                visit[i]=false;
+            }
+        }
+    }
+
+}
