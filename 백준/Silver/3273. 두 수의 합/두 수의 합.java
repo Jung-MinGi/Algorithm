247b75b27594355506e1eb1a249a19a2e8ad03aa
MinGi-Jung <118701129+Jung-MinGi@users.noreply.github.com>
2023-07-21 16:26:12 +0900

    [Silver III] Title: 두 수의 합, Time: 356 ms, Memory: 25640 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.java
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.java
@@ -1,30 +1,37 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashSet;
-import java.util.StringTokenizer;
-
+import java.util.*;
 
 public class Main {
+    static int[] arr;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
         int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[1000001];
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        HashSet<Integer> hs = new HashSet<>();
-        for(int i=0; i<n; i++){
-            int a = Integer.parseInt(st.nextToken());
-            arr[a] =1;
-            hs.add(a);
+        arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
         int m = Integer.parseInt(br.readLine());
-        int answer=0;
-        for(int a: hs){
+        int start = 0;
+        int end = n - 1;
+        int cnt = 0;
+        Arrays.sort(arr);
+        while (start < end) {
+            int sum = arr[start] + arr[end];
 
-            if(0 < m-a && m-a <= 1000000  && arr[m-a] == 1){
-             answer++;
-            }
+            if (sum >= m) end--;
+            else if(sum < m) start++;
+
+            if(sum == m )cnt++;
         }
-        System.out.println(answer/2);
+        System.out.println(cnt);
+
     }
+
 }
+
+
