6426d0aeef4defff54d3eadf5a0da0d702faa2ce
MinGi-Jung <118701129+Jung-MinGi@users.noreply.github.com>
2023-08-02 11:51:05 +0900

    [Silver IV] Title: 괄호, Time: 128 ms, Memory: 14336 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -1,30 +1,44 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Stack;
 import java.util.StringTokenizer;
-
 
 public class Main {
 
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-      int n = Integer.parseInt(br.readLine());
-      StringBuilder sb = new StringBuilder();
-      for(int i=0; i<n; i++){
-          String str = br.readLine();
-          while(str.contains("()")){
-              str = str.replace("()","");
-          }
-          if(str.length()==0){
-              sb.append("YES").append('\n');
-          }else{
-              sb.append("NO").append('\n');
-          }
-      }
-        System.out.println(sb);
-
-
-    }
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        Stack<Integer> stack;
+        StringBuilder sb = new StringBuilder();
+        while(n-->0){
+            boolean flag=true;
+            st= new StringTokenizer(br.readLine());
+            stack = new Stack<>();
+            while(st.hasMoreTokens()){
+                String a = st.nextToken();
+                for(int i=0; i<a.length(); i++){
+                    char s = a.charAt(i);
+                    if(s=='('){
+                        stack.push(1);
+                    }else{
+                        if(stack.size()==0){
+                            flag=false;
+                            break;
+                        }
+                        Integer pop = stack.pop();
+                    }
+                }
+            }
+            if(flag && stack.size()==0){
+                sb.append("YES");
+            }else{
+                sb.append("NO");
+            }
+            sb.append('\n');
+        }
+        System.out.println(sb);    }
 
 }
