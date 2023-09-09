16021ddf810de1491db4fa35aeaff5a61147937e
MinGi-Jung <118701129+Jung-MinGi@users.noreply.github.com>
2023-09-09 14:59:53 +0900

    [Silver III] Title: N과 M (5), Time: 320 ms, Memory: 29668 KB -BaekjoonHub

--- 백준/Silver/15654. N과 M （5）/N과 M （5）.java
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -1,54 +1,55 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
+
 public class Main {
 
-    public static int[] arr;
-    public static boolean[] visit;
-    static int n,m;
-    static int[] inputArr;
-    public static StringBuilder sb = new StringBuilder();
+    static int[] arr;
+    static int[] dp;
+    static boolean[] visit;
+    static StringTokenizer st;
+    static StringBuilder sb;
+
+    static int N;
 
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        arr = new int[m];
-        visit = new boolean[n+1];
-        inputArr = new int[n];
         st = new StringTokenizer(br.readLine());
-        for(int i=0; i<n; i++){
-            inputArr[i]=Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
-        Arrays.sort(inputArr);
-        dfs(0);
+        Arrays.sort(arr);
+        visit = new boolean[arr[arr.length-1]+1];
+        dp = new int[M];
+        int depth = 0;
+        sb = new StringBuilder();
+        backTracking(depth, M);
         System.out.println(sb);
     }
-    static void dfs(int depth){
-
-
-        if(depth == m){//재귀 탈출
-            for (int i : arr) {
+    static void backTracking(int depth, int M) {
+        if (depth == M) {
+            for (int i : dp) {
                 sb.append(i).append(' ');
             }
             sb.append('\n');
             return;
         }
-
-        for (int i = 0; i<inputArr.length; i++) {
-            if(!visit[i]){
-                arr[depth]=inputArr[i];
-                visit[i]=true;
-                dfs(depth+1);
-                visit[i]=false;
+        for (int i = 0; i < N; i++) {
+            if (!visit[arr[i]]) {
+                dp[depth] = arr[i];
+                visit[arr[i]] = true;
+                backTracking(depth + 1, M);
+                visit[arr[i]] = false;
             }
         }
     }
 }
+
+
