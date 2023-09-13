cd504d5bbcfcd7f56862c822040500b8d1a57401
MinGi-Jung <118701129+Jung-MinGi@users.noreply.github.com>
2023-09-13 12:42:20 +0900

    [Silver II] Title: 외판원 순회 2, Time: 308 ms, Memory: 14520 KB -BaekjoonHub

--- 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.java
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.java
@@ -25,35 +25,28 @@ public class Main {
             }
         }
         visit = new boolean[n + 1];
-        dp = new int[n];
-        backTracking(0);
+//        dp = new int[n];
+        for (int i = 1; i <= n; i++) {
+            visit[i] = true;
+            backTracking(i,i,0,0);
+            visit[i]=false;
+        }
         System.out.println(m);
     }
 
-    static void backTracking(int depth) {
-        if (depth == n) {
-            int sum=0;
-           for(int i=0; i<dp.length; i++){
-
-               if(i==dp.length-1){
-                   int x = arr[dp[i]][dp[0]];
-                   if(x==0) return;
-                   sum+=x;
-                   continue;
-               }
-               int a = arr[dp[i]][dp[i + 1]];
-               if(a==0) return;
-               sum+=a;
-           }
-           m = Math.min(m,sum);
+    static void backTracking(int startIndex, int idx, int sum, int depth) {
+        if (depth == n-1) {
+            if(arr[idx][startIndex]!=0){
+                sum+=arr[idx][startIndex];
+                m = Math.min(m, sum);
+            }
             return;
         }
         for (int i = 1; i <= n; i++) {
-            if (!visit[i]) {
-                visit[i] = true;
-                dp[depth] =i;
-                backTracking(depth + 1);
-                visit[i] = false;
+            if(!visit[i] && arr[idx][i]!=0){
+                visit[i]=true;
+                backTracking(startIndex,i,sum+arr[idx][i],depth+1);
+                visit[i]=false;
             }
         }
     }
