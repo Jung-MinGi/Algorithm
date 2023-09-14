c3160e9416466d014131f29d8e7135f67f6e5040
MinGi-Jung <118701129+Jung-MinGi@users.noreply.github.com>
2023-09-14 15:43:18 +0900

    [Silver III] Title: 퇴사, Time: 128 ms, Memory: 14260 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.java
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -1,84 +1,39 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-
 public class Main {
-    static class Node {
-        int day;
-        int price;
+    private static int n, answer = 0;
+    private static int[] T, P;
 
-        public int getDay() {
-            return day;
-        }
-
-        public int getPrice() {
-            return price;
-        }
-
-        public Node(int day, int price) {
-            this.day = day;
-            this.price = price;
-        }
-    }
-
-    static Node[] arr;
-    static Node[] dp;
-    static boolean[] visit;
-    static StringTokenizer st;
-    static StringBuilder sb = new StringBuilder();
-    static int n;
-    static int m = 0;
-
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
         n = Integer.parseInt(br.readLine());
-        arr = new Node[n];
-        dp = new Node[n];
-        visit = new boolean[n];
+
+        T = new int[n];
+        P = new int[n];
+
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-            arr[i] = new Node(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+
+            T[i] = Integer.parseInt(st.nextToken());
+            P[i] = Integer.parseInt(st.nextToken());
         }
-        for (int i = 0; i < n; i++) {
-            visit[i] = true;
-            if (arr[i].getDay() + i == n) {
-                backTracking(arr[i].getDay()+i,arr[i].getPrice(),n);
-            }
-            if (arr[i].getDay() + i > n) {
-                backTracking(arr[i].getDay()+i,0,arr[i].getDay()+i);
-            }
-            if (arr[i].getDay() + i < n) {
-                backTracking(arr[i].getDay()+i,arr[i].getPrice(),arr[i].getDay()+i);
-            }
-            visit[i]=false;
-        }
-        System.out.println(m);
+
+        dfs(0, 0);
+        System.out.println(answer);
     }
 
-    static void backTracking(int idx, int sum, int depth) {
-        if (depth >= n) {
-            m = Math.max(m, sum);
-            return;
-        }
-        for (int i = idx; i < n; i++) {
-            Node node = arr[i];
-            if (!visit[i]) {
-                visit[i]=true;
-                if (node.getDay() + i == n) {
-                    backTracking(node.getDay()+i,sum+node.getPrice(),n);
-                }
-                if (node.getDay() + i > n) {
-                    backTracking(node.getDay()+i,sum,node.getDay()+i);
-                }
-                if (node.getDay() + i < n) {
-                    backTracking(node.getDay()+i,sum+node.getPrice(),node.getDay()+i);
-                }
-                visit[i] = false;
-            }
-        }
+    private static void dfs(int index, int value) {
+       if(index>=n){
+           answer = Math.max(answer,value);
+           return;
+       }
+       if(index+T[index]<=n) dfs(index+T[index],value+P[index]);
+       else dfs(index+T[index],value);
+       
+       dfs(index+1,value);
     }
 }
-
-
