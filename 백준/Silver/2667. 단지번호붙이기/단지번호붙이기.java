27a171e6a75617814d9976c471c0e6025d1f7263
MinGi-Jung <118701129+Jung-MinGi@users.noreply.github.com>
2023-09-21 15:14:28 +0900

    [Silver I] Title: 단지번호붙이기, Time: 136 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -8,7 +8,7 @@ import java.util.stream.Stream;
 
 public class Main {
     static Integer[][] arr;
-    static ArrayList<Pair> dp = new ArrayList<>();
+    static ArrayList<int[]> dp = new ArrayList<>();
     static boolean[][] visit;
     static int n, m;
     static boolean flag = false;
@@ -45,44 +45,22 @@ public class Main {
     static void bfs(int x, int y) {
         int[] mv1 = new int[]{1, -1, 0, 0};
         int[] mv2 = new int[]{0, 0, 1, -1};
-        Queue<Pair> q = new LinkedList<>();
+        Queue<int[]> q = new LinkedList<>();
         visit[x][y] = true;
-        q.add(new Pair(x,y));
-
+        q.add(new int[]{x, y});
         while (!q.isEmpty()) {
-            Pair poll = q.poll();
+            int[] poll = q.poll();
             dp.add(poll);
             for (int i = 0; i < 4; i++) {
-                int nx = poll.x + mv1[i];
-                int ny = poll.y + mv2[i];
+                int nx = poll[0] + mv1[i];
+                int ny = poll[1] + mv2[i];
                 if (nx >= 0 && nx < n && ny >= 0 && ny < n) {
-                    if (arr[nx][ny] == 1&&!visit[nx][ny]) {
-                        q.add(new Pair(nx,ny));
+                    if (arr[nx][ny] == 1 && !visit[nx][ny]) {
+                        q.add(new int[]{nx, ny});
                         visit[nx][ny] = true;
                     }
                 }
             }
         }
     }
-
-    static class Pair {
-        int x;
-        int y;
-
-        public Pair(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-
-        public int getX() {
-            return x;
-        }
-
-        public int getY() {
-            return y;
-        }
-
-
-    }
-
 }
