b5730a0f0bac2d219ad5856135cb854a946e9ca3
MinGi-Jung <118701129+Jung-MinGi@users.noreply.github.com>
2023-10-15 13:16:25 +0900

    [Silver III] Title: 퇴사, Time: 124 ms, Memory: 14260 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.java
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -1,39 +1,39 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.StringTokenizer;
 
-public class Main {
-    private static int n, answer = 0;
-    private static int[] T, P;
 
-    public static void main(String[] args) throws Exception {
+class Main {
+    static int n, m;
+    static Integer[][] arr;
+    static Integer[] dp;
+    static boolean[] visit;
+
+    static int max = -1;
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-
         n = Integer.parseInt(br.readLine());
-
-        T = new int[n];
-        P = new int[n];
-
+        StringTokenizer st;
+        arr = new Integer[n][2];
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-
-            T[i] = Integer.parseInt(st.nextToken());
-            P[i] = Integer.parseInt(st.nextToken());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
         }
-
-        dfs(0, 0);
-        System.out.println(answer);
+        soultion(0, 0);
+        System.out.println(max);
     }
 
-    private static void dfs(int index, int value) {
-       if(index>=n){
-           answer = Math.max(answer,value);
-           return;
-       }
-       if(index+T[index]<=n) dfs(index+T[index],value+P[index]);
-       else dfs(index+T[index],value);
-       
-       dfs(index+1,value);
+
+    static void soultion(int idx, int sum) {
+        for (int i = idx; i < n; i++) {
+            soultion(i + arr[i][0], sum + arr[i][1]);
+        }
+        if (idx > n) return;
+        max = Math.max(max, sum);
     }
 }
