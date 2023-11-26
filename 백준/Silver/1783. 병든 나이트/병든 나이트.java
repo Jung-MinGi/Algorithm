d690764e96d0b39738e5c00e9b2ce7b9f2b3a30d
MinGi-Jung <118701129+Jung-MinGi@users.noreply.github.com>
2023-11-26 10:24:38 +0900

    [Silver III] Title: 병든 나이트, Time: 128 ms, Memory: 14196 KB -BaekjoonHub

---
+++ 백준/Silver/1783. 병든 나이트/병든 나이트.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int answer=0;
+        if (n == 1) answer=1;
+        else if (n == 2) answer=Math.min(4,(m+1)/2);
+        else{
+            if(m>=7){
+                answer = m-2;
+            }else{
+                answer = Math.min(4,m);
+            }
+        }
+        System.out.println(answer);
+    }
+}
+
+
