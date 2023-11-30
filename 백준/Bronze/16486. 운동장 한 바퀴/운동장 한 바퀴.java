bfaeca36f0d5907faa6f875442c2056862a441d7
MinGi-Jung <118701129+Jung-MinGi@users.noreply.github.com>
2023-11-30 21:14:43 +0900

    [Bronze IV] Title: 운동장 한 바퀴, Time: 120 ms, Memory: 14220 KB -BaekjoonHub

---
+++ 백준/Bronze/16486. 운동장 한 바퀴/운동장 한 바퀴.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int x = Integer.parseInt(br.readLine());
+        int y = Integer.parseInt(br.readLine());
+        double c = Math.PI*2*y;
+        System.out.println((x*2)+c);
+    }
+}
+
+
