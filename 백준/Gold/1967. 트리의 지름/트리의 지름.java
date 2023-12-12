bb95a37afcc3b6653d0a387eb053c5dcd2fff529
MinGi-Jung <118701129+Jung-MinGi@users.noreply.github.com>
2023-12-12 17:29:48 +0900

    [Gold IV] Title: 트리의 지름, Time: 2536 ms, Memory: 125568 KB -BaekjoonHub

--- 백준/Gold/1967. 트리의 지름/트리의 지름.java
+++ 백준/Gold/1967. 트리의 지름/트리의 지름.java
@@ -1,53 +1,58 @@
+import org.w3c.dom.Node;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 
 class Main {
-
-    static int n;
     static ArrayList<Node>[] tree;
-    static boolean[] visit = new boolean[0];
-    static StringTokenizer st;
-    static int max = Integer.MIN_VALUE;
+    static boolean[] visit;
+    static int max = 0;
 
     static class Node {
         int data;
-        int weight;
+        int val;
 
-        public Node(int data, int weight) {
+        public Node(int data, int val) {
             this.data = data;
-            this.weight = weight;
+            this.val = val;
         }
     }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
-        tree = new ArrayList[n + 1];
+        int n = Integer.parseInt(br.readLine());
+        tree = new ArrayList[n+1];
         for (int i = 0; i <= n; i++) {
             tree[i] = new ArrayList<>();
         }
+
         for (int i = 1; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
-            int parent = Integer.parseInt(st.nextToken());
-            int child = Integer.parseInt(st.nextToken());
-            int weight = Integer.parseInt(st.nextToken());
-            tree[parent].add(new Node(child, weight));
-            tree[child].add(new Node(parent, weight));
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            tree[a].add(new Node(b, c));
+            tree[b].add(new Node(a, c));
         }
 
         for (int i = 1; i <= n; i++) {
             visit = new boolean[n + 1];
-            dfs(i, 0);
+            dfs(0, i);
         }
         System.out.println(max);
+
     }
-    static void dfs(int idx, int sum) {
-        visit[idx] = true;
-        for (Node node : tree[idx]) {
+
+    static void dfs(int sum, int n) {
+        visit[n] = true;
+
+        for (Node node : tree[n]) {
             if (!visit[node.data]) {
-                dfs(node.data, sum + node.weight);
+                dfs(sum + node.val, node.data);
             }
         }
         max = Math.max(max, sum);
