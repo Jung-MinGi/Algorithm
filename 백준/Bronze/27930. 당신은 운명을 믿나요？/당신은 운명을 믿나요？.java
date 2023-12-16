2687b96af640cf59af08de22d63336f022542c12
MinGi-Jung <118701129+Jung-MinGi@users.noreply.github.com>
2023-12-16 11:44:56 +0900

    [Bronze I] Title: 당신은 운명을 믿나요?, Time: 200 ms, Memory: 19516 KB, Score: 10 point -BaekjoonHub

---
+++ 백준/Bronze/27930. 당신은 운명을 믿나요？/당신은 운명을 믿나요？.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Character[] korea = new Character[]{'K', 'O', 'R', 'E', 'A'};
+        Character[] yonsei = new Character[]{'Y', 'O', 'N', 'S', 'E', 'I'};
+        int koreaIdx=0;
+        int yonseiIdx=0;
+        char[] charArray = br.readLine().toCharArray();
+        for (int i = 0; i < charArray.length; i++) {
+            char c = charArray[i];
+            if(c==korea[koreaIdx]){
+                koreaIdx++;
+                if(korea.length==koreaIdx){
+                    System.out.println("KOREA");
+                    return;
+                }
+            }
+            else if(c==yonsei[yonseiIdx]){
+                yonseiIdx++;
+                if(yonsei.length==yonseiIdx){
+                    System.out.println("YONSEI");
+                    return;
+                }
+            }
+        }
+
+    }
+
+}
