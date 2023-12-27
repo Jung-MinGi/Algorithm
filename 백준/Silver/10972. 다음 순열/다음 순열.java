73720f23757751b63037005208ac364fb74e0686
MinGi-Jung <118701129+Jung-MinGi@users.noreply.github.com>
2023-12-27 23:28:29 +0900

    [Silver III] Title: 다음 순열, Time: 172 ms, Memory: 16100 KB -BaekjoonHub

--- 백준/Silver/10972. 다음 순열/다음 순열.java
+++ 백준/Silver/10972. 다음 순열/다음 순열.java
@@ -1,58 +1,49 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
-
 
 public class Main {
 
-    static int[] arr;
-    static int[] dp;
-    static boolean[] visit;
-    static StringTokenizer st;
-    static StringBuilder sb = new StringBuilder();
-    static LinkedHashSet<String> set = new LinkedHashSet<>();
-    static int N;
-    static int M;
-    static boolean flag = false;
-    static String answer = "";
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        st = new StringTokenizer(br.readLine());
-        arr = new int[N + 1];
-        visit = new boolean[N + 1];
-        dp = new int[N];
-        for (int i = 1; i <= N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        String[] s = br.readLine().split(" ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(s[i]);
         }
-        soultion(arr);
-
-
+       if(nextPermutation(arr)){
+           StringBuilder sb = new StringBuilder();
+           for (int i : arr) {
+               sb.append(i).append(' ');
+           }
+           System.out.println(sb);
+       }else{
+           System.out.println(-1);
+       }
     }
 
-    static void soultion(int[] a) {
-        for (int i = a.length - 1; i > 1; i--) {
-            if (a[i] > a[i - 1]) {
-                for (int j = a.length - 1; j >= i; j--) {
-                    if (a[i - 1] < a[j]) {
-                        int temp = a[i - 1];
-                        a[i - 1] = a[j];
-                        a[j] = temp;
-                        break;
-                    }
-                }
-                Arrays.sort(a, i, a.length);
-                for (int k : a) {
-                    if (k == 0) continue;
-                    System.out.print(k + " ");
-                }
-                return;
-            }
+    static boolean nextPermutation(int[] a) {
+        int i = a.length - 1;
+        while (i > 0 && a[i - 1] >= a[i]) i--;
+        if (i <= 0) return false;
+
+        int j = a.length - 1;
+
+        while (a[i - 1] >= a[j]) {
+            j--;
         }
-        System.out.println(-1);
+        int tmp = a[j];
+        a[j] = a[i - 1];
+        a[i - 1] = tmp;
+        j=a.length-1;
+        while (i<j) {
+            tmp = a[i];
+            a[i] = a[j];
+            a[j] = tmp;
+            i++;
+            j--;
+        }
+        return true;
     }
 }
-
-
