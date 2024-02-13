f60f760b669cb6971dedcb0c5faaddb67a1b2fb0
MinGi-Jung <118701129+Jung-MinGi@users.noreply.github.com>
2024-02-13 11:06:31 +0900

    [Bronze I] Title: 옷걸이, Time: 616 ms, Memory: 83112 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/27951. 옷걸이/옷걸이.java
+++ 백준/Bronze/27951. 옷걸이/옷걸이.java
@@ -1,65 +1,59 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
     static int[] arr;
-    static int top = 0;
-    static int bottom = 0;
 
+    static String[] answer;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         arr = new int[n];
+        answer = new String[n];
+        int shirt = 0;
+        int pant = 0;
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
-            if (arr[i] == 1) top++;
-            if (arr[i] == 2) bottom++;
         }
         st = new StringTokenizer(br.readLine());
-        int shirts=Integer.parseInt(st.nextToken());
-        int pants=Integer.parseInt(st.nextToken());
-        StringBuilder sb = new StringBuilder();
-        for (int i : arr) {
-            if (i == 1) {
-                if(shirts!=0){
-                    sb.append("U");
-                    shirts--;
-                    top--;
-                }
-            } else if (i == 2) {
-                if(pants!=0){
-                    sb.append("D");
-                    pants--;
-                    bottom--;
-                }
-            } else {
-                if(shirts > pants){
-                    sb.append("U");
-                    shirts--;
-                }else if(shirts < pants){
-                    sb.append("D");
-                    pants--;
-                }else{//같은 경우
-                    if(top>bottom){
+        shirt = Integer.parseInt(st.nextToken());
+        pant = Integer.parseInt(st.nextToken());
 
-                        sb.append("D");
-                        pants--;
-                    }else{
-                        sb.append("U");
-                        shirts--;
-                    }
+        for (int i=0;i<n;i++) {
+            if(arr[i]==1&&shirt!=0){
+                answer[i]="U";
+                shirt--;
+            }
+            else if(arr[i]==2&&pant!=0){
+                answer[i]="D";
+                pant--;
+            }
+        }
+        for (int i=0;i<n;i++) {
+            if(arr[i]!=3)continue;
+            if(answer[i]==null){
+                if(shirt!=0){
+                    answer[i]="U";
+                    shirt--;
+                }else if(pant!=0){
+                    answer[i]="D";
+                    pant--;
                 }
             }
         }
-if(sb.toString().length()!=n){
-    System.out.println("NO");
-}else{
-    System.out.println("YES");
-    System.out.println(sb);
-}
+        StringBuilder sb = new StringBuilder();
+        for (String s : answer) {
+            if(s==null)continue;
+            sb.append(s);
+        }
+        if(sb.toString().length()==n){
+            System.out.println("YES");
+            System.out.println(sb);
+        }else System.out.println("NO");
     }
 }
