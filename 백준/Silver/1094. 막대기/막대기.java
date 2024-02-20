5fe921fcb205a423b2e15efc97be2628e63b32a4
MinGi-Jung <118701129+Jung-MinGi@users.noreply.github.com>
2024-02-20 10:08:31 +0900

    [Silver V] Title: 막대기, Time: 120 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Silver/1094. 막대기/막대기.java
+++ 백준/Silver/1094. 막대기/막대기.java
@@ -1,28 +1,11 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
 
 
 public class Main {
-    static int[] arr = new int[]{1, 2, 4, 8, 16, 32, 64};
-    static boolean flag = false;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        soulution( 0, 0, 0, Integer.parseInt(br.readLine()));
-    }
-
-    static void soulution(int depth, int len, int cnt, int n) {
-        if(len>64)return;
-        if (len == n) {
-            flag = true;
-            System.out.println(cnt);
-            return;
-        }
-        if (depth == 7) return;
-        if (flag) return;
-        soulution(depth + 1, len + arr[depth], cnt + 1, n);
-        soulution(depth + 1, len, cnt, n);
+        System.out.println(Integer.bitCount(Integer.parseInt(br.readLine())));
     }
 }
