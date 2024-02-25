4bc530d1941afc9214faf22a98a2fe0065e56580
MinGi-Jung <118701129+Jung-MinGi@users.noreply.github.com>
2024-02-25 10:42:42 +0900

    [Silver V] Title: 소수, Time: 140 ms, Memory: 14092 KB -BaekjoonHub

---
+++ 백준/Silver/1312. 소수/소수.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int n, m, k;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+        int answer=0;
+        for(int i=0;i<k;i++){
+            n=(n%m)*10;
+            answer = n/m;
+        }
+        System.out.println(answer);
+    }
+}
