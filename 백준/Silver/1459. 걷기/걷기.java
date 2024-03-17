32ce5f02ad8e260852f9f383b70fc0ad9ab91459
MinGi-Jung <118701129+Jung-MinGi@users.noreply.github.com>
2024-03-17 10:57:57 +0900

    [Silver IV] Title: 걷기, Time: 128 ms, Memory: 14268 KB -BaekjoonHub

--- 백준/Silver/1459. 걷기/걷기.java
+++ 백준/Silver/1459. 걷기/걷기.java
@@ -3,42 +3,22 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static long min = 0;
-
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int a = Integer.parseInt(st.nextToken());
-        int b = Integer.parseInt(st.nextToken());
-        int x = Integer.parseInt(st.nextToken());
-        int y = Integer.parseInt(st.nextToken());
-        boolean flag = (a + b) % 2 == 0;
-        int tmpA=a;
-        int tmpB=b;
-        int k=0;
-        min = (long) (a + b) * x;
-        long prev = min;
-        while(a!=0&&b!=0){
-            a--;
-            b--;
-            k++;
-            long l = prev - (x * 2L) + y;
-            min = Math.min(min, l);
-            prev = l;
-        }
-        if(flag){
-            min = Math.min(min,(long) Math.max(tmpA, tmpB) * y);
-        }else min = Math.min(min,(((long) Math.max(tmpA, tmpB)-1) * y)+x);
+        long a = Long.parseLong(st.nextToken());
+        long b = Long.parseLong(st.nextToken());
+        long x = Long.parseLong(st.nextToken());
+        long y = Long.parseLong(st.nextToken());
+        long ret1 =  (a + b) * x;
+        long ret2 = 0;
+        long ret3 = 0;
 
-        System.out.println(min);
-//
-//        for (int i = 1; i <= a; i++) {
-//            long l = prev - (x * 2L) + y;
-//            System.out.println("l = " + l);
-//            min = Math.min(min, l);
-//            prev = l;
-//        }
-//        if ((a + b) % 2 == 0) min = Math.min(min, (long) Math.max(a, b) * y);
-//        System.out.println(min);
+        if ((a + b) % 2 == 0) ret2 =  Math.max(a, b) * y;
+        else {
+            ret2 =  (Math.max(a, b) - 1) *y+x;
+        }
+        ret3 = (Math.min(a, b) * y) + (Math.abs(a - b) * x);
+        System.out.println(Math.min(ret1,Math.min(ret2,ret3)));
     }
 }
