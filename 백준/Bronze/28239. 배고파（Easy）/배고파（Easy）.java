caae6189e52244013d43036e2016d682c86976c4
MinGi-Jung <118701129+Jung-MinGi@users.noreply.github.com>
2024-03-17 12:34:07 +0900

    [Bronze I] Title: 배고파(Easy), Time: 596 ms, Memory: 21724 KB -BaekjoonHub

--- 백준/Bronze/28239. 배고파（Easy）/배고파（Easy）.java
+++ 백준/Bronze/28239. 배고파（Easy）/배고파（Easy）.java
@@ -2,27 +2,28 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class Main {
-    static final long MAX=1000000000000000000L;
+    static final long MAX = 1000000000000000000L;
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n =Integer.parseInt(br.readLine());
-        while (n-->0){
+        int n = Integer.parseInt(br.readLine());
+        while (n-- > 0) {
             long ret = Long.parseLong(br.readLine());
             long a = 0;
             long b = 0;
-            boolean flag=false;
-            for(int i=0;;i++){
-                for(int j=0;;j++){
-                    a=(long)Math.pow(2,i);
-                    b=(long)Math.pow(2,j);
-                    if(a+b==ret){
-                        System.out.println(i+" "+j);
-                        flag=true;
+            boolean flag = false;
+            for (int i = 0; ; i++) {
+                for (int j = 0; ; j++) {
+                    a = 1L << i;
+                    b = 1L << j;
+                    if (a + b == ret) {
+                        System.out.println(i + " " + j);
+                        flag = true;
                         break;
                     }
-                    if(a+b>MAX)break;
+                    if (a + b > MAX) break;
                 }
-                if(flag)break;
+                if (flag) break;
             }
         }
 
