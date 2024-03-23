6faca2924149103d4a19ac684ae14764c3d7a487
MinGi-Jung <118701129+Jung-MinGi@users.noreply.github.com>
2024-03-23 16:59:13 +0900

    [Silver IV] Title: blobyum, Time: 284 ms, Memory: 24696 KB -BaekjoonHub

--- 백준/Silver/24499. blobyum/blobyum.java
+++ 백준/Silver/24499. blobyum/blobyum.java
@@ -3,7 +3,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-
 public class Main {
 
     public static void main(String[] args) throws IOException {
@@ -11,21 +10,31 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-        int[] arr = new int[n * 2];
+        int[] arr = new int[n];
+        int[] preFixSum = new int[n];
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
-            arr[n + i] = arr[i];
+            if(i==0)preFixSum[i]=arr[i];
+            else preFixSum[i]=preFixSum[i-1]+arr[i];
+        }
+        if(n==m){
+            System.out.println(preFixSum[n-1]);
+            return;
         }
         int answer=0;
-        int max = 0;
-        for (int i = 0; i < m - 1; i++) {
-            max += arr[i];
-        }
-        for(int i=m-1;i<arr.length;i++){
-            max+=arr[i];
-            answer = Math.max(answer,max);
-            max-=arr[i-m+1];
+        for(int i=0;i<m;i++)answer+=arr[i];
+        for(int i=1;i<arr.length;i++){
+
+            if(i+m<=n){
+                if(i+m==n){
+                    answer = Math.max(answer,preFixSum[i+m-1]-preFixSum[i-1]);
+                }
+                else answer = Math.max(answer,preFixSum[i+m]-preFixSum[i]);
+            }
+            else{
+                answer = Math.max(answer,preFixSum[n-1]-preFixSum[i-1]+preFixSum[(i+m)%n-1]);
+            }
         }
         System.out.println(answer);
     }
