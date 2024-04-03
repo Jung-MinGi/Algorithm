f365db64da2d30094f9aaadab0522389868d4097
MinGi-Jung <118701129+Jung-MinGi@users.noreply.github.com>
2024-04-03 12:20:15 +0900

    [Bronze IV] Title: 現れている数字 (Appearing Numbers), Time: 124 ms, Memory: 14200 KB -BaekjoonHub

---
+++ 백준/Bronze/31609. 現れている数字 （Appearing Numbers）/現れている数字 （Appearing Numbers）.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) arr[i] = Integer.parseInt(st.nextToken());
+        Arrays.sort(arr);
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            if (i == 0) sb.append(arr[i]).append('\n');
+            else {
+                if (arr[i] != arr[i - 1]) sb.append(arr[i]).append('\n');
+            }
+        }
+        System.out.println(sb);
+    }
+}
+
+
