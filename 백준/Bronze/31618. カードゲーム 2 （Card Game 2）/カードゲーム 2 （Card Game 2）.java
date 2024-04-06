387d82a74c75375c714b190d6eba1609ba405778
MinGi-Jung <118701129+Jung-MinGi@users.noreply.github.com>
2024-04-06 16:30:18 +0900

    [Bronze II] Title: カードゲーム 2 (Card Game 2), Time: 356 ms, Memory: 31116 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/31618. カードゲーム 2 （Card Game 2）/カードゲーム 2 （Card Game 2）.java
+++ 백준/Bronze/31618. カードゲーム 2 （Card Game 2）/カードゲーム 2 （Card Game 2）.java
@@ -9,17 +9,18 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
-        boolean[] arr = new boolean[101];
+        boolean[] arr = new boolean[200001];
         StringTokenizer st = new StringTokenizer(br.readLine());
         while (st.hasMoreTokens()) arr[Integer.parseInt(st.nextToken())] = true;
-        for (int i = 1; i <= n; i++) {
-            if (i + 6 <= 100 && arr[i] && arr[i + 3] && arr[i + 6]) {
+        for (int i = 1; i < arr.length; i++) {
+            if (i + 6 <= 200000 && arr[i] && arr[i + 3] && arr[i + 6]) {
+                //System.out.println(i);
                 System.out.println("Yes");
                 return;
             }
         }
         System.out.println("No");
-    }
 
+    }
 }
 
