b73afd62f8526e37e44b68d3a24b5181603b5c00
MinGi-Jung <118701129+Jung-MinGi@users.noreply.github.com>
2024-04-13 15:56:35 +0900

    [level 2] Title: 중성화 여부 파악하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
+++ 프로그래머스/2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
@@ -1,8 +1,12 @@
 -- 코드를 입력하세요
-SELECT ANIMAL_ID,NAME,
-case  
-    when SEX_UPON_INTAKE='Neutered Male' then 'O'
-    when SEX_UPON_INTAKE='Spayed Female' then 'O'
-    else 'X'
-    end as '중성화'
-    from ANIMAL_INS  order by ANIMAL_ID;
+select
+ANIMAL_ID
+,name
+,case 
+when SEX_UPON_INTAKE like 'Neu%'
+then 'O'
+when SEX_UPON_INTAKE like 'Spa%'
+then 'O'
+else 'X'
+end
+from ANIMAL_INS
