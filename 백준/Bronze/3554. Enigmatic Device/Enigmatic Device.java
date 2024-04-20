5a5df79d6965b315b5f64fca9879daa88860d3db
MinGi-Jung <118701129+Jung-MinGi@users.noreply.github.com>
2024-04-20 22:13:13 +0900

    [Bronze III] Title: Enigmatic Device, Time: 4632 ms, Memory: 45708 KB -BaekjoonHub

--- 백준/Bronze/3554. Enigmatic Device/Enigmatic Device.java
+++ 백준/Bronze/3554. Enigmatic Device/Enigmatic Device.java
@@ -1,33 +1,42 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-public class Main {
-    public static void solve() {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        int[] a = new int[n];
-        for (int i = 0; i < n; i++) a[i] = scanner.nextInt();
+class Main {
+    private static final int MOD = 2010;
 
-        int m = scanner.nextInt();
-        while (m-- > 0) {
-            int k = scanner.nextInt();
-            int l = scanner.nextInt() - 1;
-            int r = scanner.nextInt() - 1;
-            if (k == 1) {
-                for (int j = l; j <= r; j++) {
-                    a[j] = (a[j] * a[j]) % 2010;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] a = new int[n + 1];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= n; i++) {
+            a[i] = Integer.parseInt(st.nextToken());
+        }
+        StringBuilder sb = new StringBuilder();
+
+        int tc = Integer.parseInt(br.readLine());
+        while (tc-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int ans = 0;
+            int l = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+            if (x == 1) {
+                for (int i = l; i <= m; i++) {
+                    a[i] = (a[i]*a[i]) % MOD;
                 }
             } else {
-                int sum = 0;
-                for (int j = l; j <= r; j++) {
-                    sum += a[j];
+                for (int i = l; i <= m; i++) {
+                    ans += a[i];
                 }
-                System.out.println(sum);
+                sb.append(ans).append('\n');
             }
         }
-        scanner.close();
+        System.out.println(sb);
+
     }
 
-    public static void main(String[] args) {
-        solve();
-    }
+
 }
