086a1e466c93a494b07a27f05a326f7f369ecaf5
MinGi-Jung <118701129+Jung-MinGi@users.noreply.github.com>
2024-05-02 21:14:49 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 148 ms, Memory: 15828 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(":fan::fan::fan:");
+        System.out.println(":fan::"+br.readLine()+"::fan:");
+        System.out.println(":fan::fan::fan:");
+    }
+}
