c66a1ae4629229567b6c3273b05aa7811c74ae96
MinGi-Jung <118701129+Jung-MinGi@users.noreply.github.com>
2024-05-08 15:37:40 +0900

    [Bronze II] Title: 팰린드롬, Time: 128 ms, Memory: 14128 KB -BaekjoonHub

---
+++ 백준/Bronze/13235. 팰린드롬/팰린드롬.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        StringBuilder sb = new StringBuilder(s).reverse();
+        System.out.println(s.equals(sb.toString()));
+    }
+}
