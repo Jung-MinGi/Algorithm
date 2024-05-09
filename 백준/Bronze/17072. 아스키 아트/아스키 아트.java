3c4ac52672e6cc45b23bc89d9e05200a9badb152
MinGi-Jung <118701129+Jung-MinGi@users.noreply.github.com>
2024-05-09 14:46:13 +0900

    [Bronze I] Title: 아스키 아트, Time: 1460 ms, Memory: 60944 KB -BaekjoonHub

---
+++ 백준/Bronze/17072. 아스키 아트/아스키 아트.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static char[][] a;
+private static int x=2040000;
+private static int y=1530000;
+private static int z=1020000;
+private static int t=510000;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        a = new char[n][m];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                int q = Integer.parseInt(st.nextToken());
+                int b = Integer.parseInt(st.nextToken());
+                int c = Integer.parseInt(st.nextToken());
+                char s;
+                int intensity = intensity(q, b, c);
+                if(intensity>=x)s='.';
+                else if(intensity>=y)s='-';
+                else if(intensity>=z)s='+';
+                else if(intensity>=t)s='o';
+                else s='#';
+                a[i][j]=s;
+            }
+        }
+        for (char[] chars : a) {
+            for (char aChar : chars) {
+                System.out.print(aChar);
+            }
+            System.out.println();
+        }
+
+    }
+
+    static int intensity(int a, int b, int c) {
+       return (a * 2126 + b * 7152 + c * 722);
+    }
+}
