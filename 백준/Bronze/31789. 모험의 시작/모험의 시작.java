6b7af676dd574b1679b1d8ede4be3b661c3ccef3
MinGi-Jung <118701129+Jung-MinGi@users.noreply.github.com>
2024-07-02 20:13:59 +0900

    [Bronze III] Title: 모험의 시작, Time: 124 ms, Memory: 14176 KB -BaekjoonHub

---
+++ 백준/Bronze/31789. 모험의 시작/모험의 시작.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int x = Integer.parseInt(st.nextToken());
+        int y = Integer.parseInt(st.nextToken());
+
+        while (n-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            if (a <= x && b > y) {
+                System.out.println("YES");
+                return;
+            }
+        }
+        System.out.println("NO");
+    }
+
+}
